b262baddb6eab5299ebd963895a23693fa914e68
Bivol <vbivol394@gmail.com>
2025-09-07 18:38:04 +0300

    conecatare suprabase

---
+++ src/app/lib/suprabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPRA_BASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
