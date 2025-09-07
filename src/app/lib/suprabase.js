9bafa621ce7196389b28c2d7794a9ae460549749
Bivol <vbivol394@gmail.com>
2025-09-07 19:30:37 +0300

    acuma chiar mere tailwindu

--- src/app/lib/suprabase.js
+++ src/app/lib/suprabase.js
@@ -1,8 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPRA_BASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabaseclient = createClient(supabaseUrl, supabaseKey)
