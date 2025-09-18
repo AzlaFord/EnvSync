b0f4904f9a8dcef6ccb97285a4a8834dbf2010ed
Bivol <vbivol394@gmail.com>
2025-09-18 16:22:15 +0300

    modifica sa paota ascepta un sigur key sau mai multe

--- src/app/lib/writesecret.js
+++ src/app/lib/writesecret.js
@@ -1,16 +1,32 @@
 import { encrypt } from "./crypto";
+import createClient from "@/utils/supabase/server";
 
-export async function addEnvVars(repo_id, user_id, secrets) {
+export async function addEnvVars(repo_id, user_id, secretsInput) {
+  const supabase = await createClient();
 
-    for (const s of secrets) {
-        const { cipher, iv, tag } = encrypt(s.value);
-        await supabase.from('env_vars').insert({
-        repo_id,
-        key_name: s.key_name,
-        encrypted_value: cipher,
-        iv,
-        tag,
-        created_by: user_id,
-        });
-    }
+  let secretsArray = [];
+
+  if (Array.isArray(secretsInput)) {
+    secretsArray = secretsInput;
+  } else if (typeof secretsInput === "object" && secretsInput !== null) {
+    secretsArray = Object.entries(secretsInput).map(([key_name, value]) => ({
+      key_name,
+      value,
+    }));
+  } else {
+    throw new Error("Invalid secrets format");
+  }
+
+  for (const s of secretsArray) {
+    const { cipher, iv, tag } = encrypt(s.value);
+
+    await supabase.from("env_vars").insert({
+      repo_id,
+      key_name: s.key_name,
+      encrypted_value: cipher,
+      iv,
+      tag,
+      created_by: user_id,
+    });
+  }
 }
