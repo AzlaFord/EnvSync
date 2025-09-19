38c958d8cd9861d750edd09c8f982c743181be37
Bivol <vbivol394@gmail.com>
2025-09-19 10:37:12 +0300

    decryptarea

---
+++ src/app/lib/decrypt.js
@@ -0,0 +1,23 @@
+import { decrypt } from "./crypto";
+import createClient from "@/utils/supabase/server";
+
+export async function getEnvVar(repo_id, key_name, user_id) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("env_vars")
+    .select("*")
+    .eq("repo_id", repo_id)
+    .eq("key_name", key_name)
+    .single();
+
+  if (error || !data) throw new Error("Secret not found or access denied");
+
+  const decrypted = decrypt({
+    cipher: data.encrypted_value,
+    iv: data.iv,
+    tag: data.tag,
+  });
+
+  return decrypted
+}
