95de2f0f639b1db76ded20ec4e968791a1e1f814
Bivol <vbivol394@gmail.com>
2025-09-20 12:54:11 +0300

    am scos scroll bar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <html>
+      <html className='scrollbar-hide '>
         <body>{children}</body>
       </html>
     </QueryClientProvider>
--- src/app/repository-details.jsx
+++ src/app/repository-details.jsx
@@ -78,7 +78,7 @@ export function RepositoryDetails({ repositoryName,owner,userId, onBack }) {
   if (!repo) return <LoadingDetails/>
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 overflow-auto ">
       <div className="flex items-center gap-4">
         <Button variant="outline" size="sm" onClick={onBack}>
           <ArrowLeft className="h-4 w-4 mr-2" />
