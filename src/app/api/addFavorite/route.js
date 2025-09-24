460a1dc65c7b424a617bf26e1c29eb9ba075b28a
Bivol <vbivol394@gmail.com>
2025-09-24 11:57:10 +0300

    facut route API pentru add to favorites

---
+++ src/app/api/addFavorite/route.js
@@ -0,0 +1,18 @@
+import addToFavorites from "@/app/lib/addToFavorites"
+import { NextResponse } from "next/server"
+
+export async function POST(request) {
+    const body = await request.json()
+    const {repo_id,repo_name,language,owner} = body
+    if(!repo_id || repo_id.length === 0){return NextResponse.json({message:" repo_id e undefined sau e gol sau null"},{status:400})}
+    if(!repo_name || repo_name.length === 0){return NextResponse.json({message:" repoName e undefined sau e gol sau null"},{status:400})}
+    if(!language || language.length === 0){return NextResponse.json({message:" language e undefined sau e gol sau null"},{status:400})}
+    if(!owner || owner.length === 0){return NextResponse.json({message:" owner e undefined sau e gol sau null"},{status:400})}
+
+    try{
+        await addToFavorites(repo_id,owner,language,repo_name)
+        return NextResponse.json({message:"a mers totul bine "},{status:201})
+    }catch(err){
+        return NextResponse.json({message:err.message},{status:500})
+    }   
+}
--- src/app/lib/addToFavorites.js
+++ src/app/lib/addToFavorites.js
@@ -1,7 +1,15 @@
 import createClient from "@/utils/supabase/server";
 
-export default async function addToFavorites(repo_id,owner,language,user_id,repo_name){
+export default async function addToFavorites(repo_id,owner,language,repo_name){
     const supabase = await createClient()
+    const {data:user,error:userError} = await supabase.auth.getUser()
+    if (userError || !user) {
+        console.error("user error:", error);
+        throw userError;
+    }
+
+    const user_id = user?.user_metadata?.user_id
+
     const {error} = await supabase
         .from("favorite")
         .insert({repo_id:repo_id,owner:owner,language:language,user_id:user_id,repo_name:repo_name})
