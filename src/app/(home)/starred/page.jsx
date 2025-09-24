6dfcd70b967445123517a7933eafe83c19dd7a30
Bivol <vbivol394@gmail.com>
2025-09-24 17:34:34 +0300

    Starred e somehow useable i guess ?

--- src/app/(home)/starred/page.jsx
+++ src/app/(home)/starred/page.jsx
@@ -8,8 +8,7 @@ import { Card, CardContent, CardHeader, CardTitle,CardAction, CardDescription } 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
-
-
+import { useState } from "react"
 const getStarred = async () =>{
     try{
         const res = await fetch("/api/getFavorite",)
@@ -21,12 +20,26 @@ const getStarred = async () =>{
 }
 
 export default function StarredPage(){
+    const [fetchAgain,setFetchAgain] = useState(false)
     const router = useRouter()
     const {data:repos,isLoading } = useQuery({
-        queryKey:["repos2"],
+        queryKey:["repos2",fetchAgain],
         queryFn: () => getStarred(),
     })
-    console.log(repos)
+
+    const removeStarred = async (id) =>{
+        try{
+            await fetch("/api/deleteFromFavoriteOne",{
+                method:"DELETE",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({repoId:id})
+            })
+        }catch(err){
+            throw err
+        }finally{
+            setFetchAgain(prev => !prev)
+        }    
+    }
     return (<>
     <div className="w-full pl-5">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
@@ -40,7 +53,7 @@ export default function StarredPage(){
                 <Card key={repo?.id} className=" flex justify-center " >
                     <CardHeader>
                         <CardTitle className="flex justify-start items-center ">
-                            <StarOff fill="orange" className="mr-3"/>
+                            <StarOff fill="orange" className=" mr-1 max-sm:hidden" onClick={()=>{removeStarred(repo?.id)}} />
                                 <Button variant="link" className="p-0 h-auto font-semibold" 
                                 onClick = {() =>{router.push(`/repositories/${repo?.repo_name}?owner=${repo?.owner}&cursor=Starred`)}}
                                 >
