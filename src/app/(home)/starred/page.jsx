204a75c2648d6e2a93efac5c69d1b78cb6c3eac9
Bivol <vbivol394@gmail.com>
2025-09-24 16:15:04 +0300

    am facut sa fie mai responsive

--- src/app/(home)/starred/page.jsx
+++ src/app/(home)/starred/page.jsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query"
 import { Separator } from "@/components/ui/separator"
-import {FolderSearch,ArrowRight} from "lucide-react"
+import {FolderSearch,ArrowRight , StarOff} from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardHeader, CardTitle,CardAction, CardDescription } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
@@ -29,16 +29,18 @@ export default function StarredPage(){
     console.log(repos)
     return (<>
     <div className="w-full pl-5">
-        <h1>Favorite</h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Starred repositories
+        </h2>
     </div>
-    <Separator/>
     <div className="w-full flex justify-center items-center p-5">
         <div className="w-full flex justify-center items-center ">
-            <div className="grid grid-cols-1 w-full gap-3">
+            <div className="grid grid-cols-1  max-md:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 w-full  gap-3">
                 {(repos?.repos||[]).map(repo=>(
                 <Card key={repo?.repo_name} className=" flex justify-center " >
                     <CardHeader>
-                        <CardTitle>
+                        <CardTitle className="flex justify-start items-center ">
+                            <StarOff fill="orange" className="mr-3"/>
                                 <Button variant="link" className="p-0 h-auto font-semibold" 
                                 onClick = {() =>{router.push(`/repositories/${repo?.repo_name}?owner=${repo?.owner}&cursor=Starred`)}}
                                 >
@@ -52,20 +54,11 @@ export default function StarredPage(){
                                 Details<ArrowRight color="white"/>
                             </Button>
                         </CardAction>
-                        <CardDescription>
-                            <div>
-                                <Badge variant="default">{repo?.language||"N/A"}</Badge>
-                            </div>
-                        </CardDescription>
                     </CardHeader>
                 </Card>
                 ))}
             </div>
         </div>
     </div>
-
-
-
-
     </>)
 }
--- src/app/lib/getAllStarred.js
+++ src/app/lib/getAllStarred.js
@@ -2,13 +2,24 @@ import createClient from "@/utils/supabase/server"
 
 export async function getRepoStared() {
     const supabase = await createClient()
-    const {data,error} =  await supabase
+    const { data, error: userError } = await supabase.auth.getUser()
+    const user = data?.user
+
+    if (userError || !user) {
+        console.error("user error:", userError);
+        throw userError || new Error("No user found")
+    }
+
+    const user_id = user.id
+    const { data: repos, error } = await supabase
         .from("favorite")
         .select("owner,repo_name,language")
+        .eq("user_id", user_id)
 
-    if(error){
+    if (error) {
         console.log(error)
         throw error
     }
-    return {message:"totul a mers bine",success:true,data}
+
+    return { message: "totul a mers bine", success: true, data: repos }
 }
