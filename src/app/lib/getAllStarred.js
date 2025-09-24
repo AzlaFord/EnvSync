44bf9935ffc6398eb4bf66f347fb7342ebfb176f
Bivol <vbivol394@gmail.com>
2025-09-24 14:14:26 +0300

    fetch de la supabase

---
+++ src/app/lib/getAllStarred.js
@@ -0,0 +1,14 @@
+import createClient from "@/utils/supabase/server"
+
+async function getRepoStared() {
+    const supabase = await createClient()
+    const {data,error} =  await supabase
+        .from("favorite")
+        .select(owner,repo_name,language)
+
+    if(error){
+        console.log(error)
+        throw error
+    }
+    return {message:"totul a mers bine",success:true,data}
+}
