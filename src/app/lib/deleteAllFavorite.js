e63f051a4a51a1b3d1b33d3818a00a8e8f1464b8
Bivol <vbivol394@gmail.com>
2025-09-24 18:00:14 +0300

    facut logica stergere pentru baza de date

---
+++ src/app/lib/deleteAllFavorite.js
@@ -0,0 +1,17 @@
+import createClient from "@/utils/supabase/server"
+
+export async function deleteAllFavorite(){
+    const supabase = await createClient()
+    const { data:{user}, error: userError } = await supabase.auth.getUser()
+    if (userError || !user) {
+        console.error("user error:", userError);
+        throw userError || new Error("No user found")
+    }
+    const {error} = supabase.from("favorite").delete().eq("user_id",user.id)
+    if(error){
+        console.error("error:", error);
+        throw error || new Error("No user found")
+    }
+    
+    return {message:"totul bine ",success:true}
+}
