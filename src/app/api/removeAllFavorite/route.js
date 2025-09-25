c1e3df119ac98c7fdf2600c99e94efe295580942
Bivol <vbivol394@gmail.com>
2025-09-25 20:15:23 +0300

    am facut route api pentru remove all for favorites

---
+++ src/app/api/removeAllFavorite/route.js
@@ -0,0 +1,14 @@
+import { deleteAllFavorite } from "@/app/lib/deleteAllFavorite";
+import { NextResponse } from "next/server";
+
+export async function DELETE(request) {
+    try{
+        const res = await deleteAllFavorite()
+        if(!res.success){
+            return NextResponse.json({message:"ceva nu a mers bine la stergearea"},{status:400})
+        }
+        return NextResponse.json({message:"a mers totul bine "},{status:203})
+    }catch(err){
+        return NextResponse.json({message:"ceava nu a mers bine la staergearea din favorite"},{status:400})
+    }
+}
