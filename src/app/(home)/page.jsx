26d93dce74b877e28bf41649edc4795248da4cab
Bivol <vbivol394@gmail.com>
2025-09-26 15:19:23 +0300

    am chimbat putin

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,16 +1,15 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle,CardFooter } from "@/components/ui/card"
-
+import { Separator } from "@/components/ui/separator"
 export default function HomePageNavBar(){
     return(<>
         <Card>
             <CardHeader>
-                <CardTitle>SALUT!</CardTitle>
-                <CardDescription>
-                    Navigeaza
+                <CardTitle className="flex justify-center scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 ">Welcome!</CardTitle>
+                <CardDescription className='scroll-m-20 text-2xl font-semibold tracking-tight ' >
+                    Navigate
+                    <Separator/>
                 </CardDescription>
             </CardHeader>
         </Card>
-    
-    
      </>)
 }
